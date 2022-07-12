d80664f3f5afa3552099b50bdd1601d7ea8f3d0a
kbiebuyck <kbiebuyck@develop-it.net>
2022-07-12 12:50:06 +0200

    feat: fill searchQuery from querystring

--- src/app/header-banner/header-banner.component.ts
+++ src/app/header-banner/header-banner.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-header-banner',
   templateUrl: './header-banner.component.html',
   styleUrls: ['./header-banner.component.scss']
 })
-export class HeaderBannerComponent implements OnInit {
+export class HeaderBannerComponent implements OnInit, OnDestroy {
+  private _destroyed = new Subject();
   options = ["test 1", "test2"];
   searchQuery = "";
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        tap(qp => this.searchQuery = qp["q"]),
+        takeUntil(this._destroyed)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed.next(null);
+    this._destroyed.complete();
   }
 
 }
